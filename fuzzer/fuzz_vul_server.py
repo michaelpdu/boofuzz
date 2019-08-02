7352021685de6c218f21fedcd5c03fbf351ca09f
dupei <dupei.dp@gpus.arcslab.com>
2019-08-02 14:57:54 +0800

    add fuzz_vul_server.py

---
+++ fuzzer/fuzz_vul_server.py
@@ -0,0 +1,24 @@
+from boofuzz import *
+
+def main():
+    target_ip = "127.0.0.1"
+    port_num = 8901
+    start_cmd = ['/home/dupei/github/boofuzz/vul_server/vul_server']
+    session = Session(
+        target=Target(
+            connection=SocketConnection(target_ip, port_num, proto='tcp'),
+            procmon=pedrpc.Client(target_ip, 26002),
+            procmon_options={"start_commands": [start_cmd]}
+        ),
+       sleep_time=1,
+    )
+
+    s_initialize(name="Request")
+    with s_block("Request-Line"):
+        s_string("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", name='Request-URI')
+
+    session.connect(s_get("Request"))
+    session.fuzz()
+
+if __name__ == "__main__":
+    main()
