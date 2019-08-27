775065f2cabc996c2ac194b66d60fddf4d8c673b
dupei <dupei.dp@gpus.arcslab.com>
2019-08-27 11:18:52 +0800

    add socket_connect

---
+++ fuzzer/socket_connect.py
@@ -0,0 +1,34 @@
+import os
+import sys
+fuzzer_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(fuzzer_dir)
+sys.path.append(root_dir)
+
+from boofuzz import *
+
+target_ip = "127.0.0.1"
+port_num = 8901
+
+def main():
+    start_cmd = ['/home/dupei/github/boofuzz/vul_server/vul_server']
+    session = Session(
+        target=Target(
+            connection=SocketConnection(target_ip, port_num, proto='tcp'),
+            procmon=pedrpc.Client(target_ip, 26002),
+            procmon_options={"start_commands": [start_cmd]}
+        ),
+       sleep_time=1,
+    )
+
+    s_initialize(name="Request")
+    with s_block("Request-Line"):
+        s_string("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", name='Request-URI')
+    s_static('\n')
+
+    session.connect(s_get("Request"))
+    session.fuzz()
+
+if __name__ == "__main__":
+    # main()
+    connection = SocketConnection(target_ip, port_num, proto='tcp')
+    connection.send('hello')
